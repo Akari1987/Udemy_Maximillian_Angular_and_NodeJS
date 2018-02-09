b301e1300785ddf3ca125ce8170ecac5ea75d5f0
Akari1987 <leaves.saint@gmail.com>
2018-02-09 22:24:40 +0900

    Lecture 28 clean up previous app.

--- routes/app.js
+++ routes/app.js
@@ -1,27 +1,30 @@
 var express = require('express');
 var router = express.Router();
-var User = require('../models/user');
+// var User = require('../models/user');
+// 
+// router.get('/', function (req, res, next) {
+//     User.findOne({}, function(err, doc) {
+//         if(err) {
+//             return res.send('Error!');
+//         }
+//         res.render('node', {email: doc.email});
+//     });
+// });
+//
+// router.post('/', function(req, res, next) {
+//     var email = req.body.email;
+//     var user = new User({
+//       firstName: 'Max',
+//       lastName: 'Schwarz',
+//       password: 'super-secret',
+//       email: email
+//     });
+//     user.save();
+//     res.redirect('/');
+// });
 
-router.get('/', function (req, res, next) {
-    User.findOne({}, function(err, doc) {
-        if(err) {
-            return res.send('Error!');
-        }
-        res.render('node', {email: doc.email});
-    });
-    // res.render('node');
-});
-
-router.post('/', function(req, res, next) {
-    var email = req.body.email;
-    var user = new User({
-      firstName: 'Max',
-      lastName: 'Schwarz',
-      password: 'super-secret',
-      email: email
-    });
-    user.save();
-    res.redirect('/');
+router.get('/', function(req, res, next) {
+    res.render('index');
 });
 
 module.exports = router;
