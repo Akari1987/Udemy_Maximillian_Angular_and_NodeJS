0955c305bedfa74cb401b3323984ed0c5f8716ba
Akari1987 <leaves.saint@gmail.com>
2018-02-21 22:24:19 +0900

    Lecture 44 Adding Input and List Components - Rebuild components structure and Passing input value to the event(save button) by #input

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,14 +1,9 @@
 import { Component } from '@angular/core';
 
-import { Message } from './messages/message.model';
-
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html',
 })
 export class AppComponent {
-    messages: Message[] = [
-        new Message('Some message', 'Max'),
-        new Message('Something else', 'Max')
-    ];
+
 }
